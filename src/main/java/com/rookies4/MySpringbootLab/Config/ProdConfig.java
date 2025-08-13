4bcef58ca78816394d90e1306a82d495077fcef4
kbs0621 <jjjustin7531@gmail.com>
2025-08-13 20:06:11 +0900

    update

---
+++ src/main/java/com/rookies4/MySpringbootLab/Config/ProdConfig.java
@@ -0,0 +1,20 @@
+package com.rookies4.MySpringbootLab.Config;
+
+
+import com.rookies4.MySpringbootLab.Config.vo.MyEnvironment;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Profile("prod")
+@Configuration
+public class ProdConfig {
+    @Bean
+    public MyEnvironment myEnvironment(){
+        return MyEnvironment.builder()
+                .mode("운영환경")
+                .build();
+
+    }
+
+}
---
+++ src/main/java/com/rookies4/MySpringbootLab/Config/TestConfig.java
@@ -0,0 +1,19 @@
+package com.rookies4.MySpringbootLab.Config;
+
+
+import com.rookies4.MySpringbootLab.Config.vo.MyEnvironment;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Profile("test")
+@Configuration
+public class TestConfig {
+    @Bean
+    public MyEnvironment myEnvironment(){
+        return MyEnvironment.builder()
+                .mode("개발환경")
+                .build();
+    }
+
+}
